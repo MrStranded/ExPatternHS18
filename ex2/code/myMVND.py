bbbe46ed8ba2bcebc690b8be115c16e5ee7b3d9f
Burgert Maximilian - burmax00 <max.burgert@unibas.ch>
2018-10-05 09:49:22 +0200

    Added pdf and logpdf again and correct

--- ex2/code/myMVND.py
+++ ex2/code/myMVND.py
@@ -8,11 +8,9 @@ class MVND:
         self.data = data
         self.mean = np.matrix(data).mean(1)
         self.cov  = np.cov(data)
-        self.prefactor = 1/(np.sqrt(np.power(2*np.pi, np.shape(self.data)[1])*np.linalg.det(self.cov)))
 
     def pdf(self, x):
-        exponent = np.power(np.e, -0.5*(x-self.data).T.dot(np.linalg.inv(self.cov)).dot(x-self.data))
-        return self.prefactor*exponent
+        return multivariate_normal.pdf(x, self.mean, self.cov)
     
     def logpdf(self, x):
-        return np.log(self.prefactor) + (-0.5*(x-self.data).T.dot(np.linalg.inv(self.cov)).dot(x-self.data))
+        return multivariate_normal.logpdf(x, self.mean, self.cov)
