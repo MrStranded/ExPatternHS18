14fd991513e2a27f0fb8471cd62b6e9330a7cb66
Burgert Maximilian - burmax00 <max.burgert@unibas.ch>
2018-10-05 09:45:30 +0200

    Added pdf and logpdf

--- ex2/code/myMVND.py
+++ ex2/code/myMVND.py
@@ -3,16 +3,16 @@ from scipy.stats import multivariate_normal
 
 
 class MVND:
-    # TODO: EXERCISE 2 - Implement mean and covariance matrix of given data
     def __init__(self, data, p=1.0):
         self.p = p
         self.data = data
         self.mean = np.matrix(data).mean(1)
         self.cov  = np.cov(data)
+        self.prefactor = 1/(np.sqrt(np.power(2*np.pi, np.shape(self.data)[1])*np.linalg.det(self.cov)))
 
-    # TODO: EXERCISE 2 - Implement pdf and logpdf of a MVND
     def pdf(self, x):
-        return None
+        exponent = np.power(np.e, -0.5*(x-self.data).T.dot(np.linalg.inv(self.cov)).dot(x-self.data))
+        return self.prefactor*exponent
     
     def logpdf(self, x):
-        return None
+        return np.log(self.prefactor) + (-0.5*(x-self.data).T.dot(np.linalg.inv(self.cov)).dot(x-self.data))
