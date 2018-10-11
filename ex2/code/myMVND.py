382d1b756e2f1ba94f2579a54caf2ecd90bab71f
Burgert Maximilian - burmax00 <max.burgert@unibas.ch>
2018-10-11 17:34:43 +0200

    fixing myMVND

--- ex2/code/myMVND.py
+++ ex2/code/myMVND.py
@@ -5,10 +5,9 @@ from scipy.stats import multivariate_normal
 class MVND:
     def __init__(self, data, p=1.0):
         self.p = p
-        a = np.array([[1, 2], [3, 4]])
         self.data = data
         self.mean = data.mean(1)
-        self.cov = np.cov(data)
+        self.cov  = np.cov(data)
 
     def pdf(self, x):
         return multivariate_normal.pdf(x, self.mean, self.cov)
