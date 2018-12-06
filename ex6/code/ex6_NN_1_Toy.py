30e16631ac3586629370a54ecd2053a9030d80b5
Burgert Maximilian - burmax00 <max.burgert@unibas.ch>
2018-12-06 12:28:55 +0100

    Fixing yesterdays issue

--- ex6/code/ex6_NN_1_Toy.py
+++ ex6/code/ex6_NN_1_Toy.py
@@ -21,21 +21,15 @@ def toyNetwork():
     w4 = 0.1
     w5 = 0.8
     w6 = 0.3
-    w7 = 0.5
-    w8 = 0.9
-    w9 = 0.2
-
-    w135 = Variable(torch.Tensor([w1, w3, w5]), requires_grad=True)
-    w246 = Variable(torch.Tensor([w2, w4, w6]), requires_grad=True)
-    w789 = Variable(torch.Tensor([w7, w8, w9]), requires_grad=True)
+    w7 = Variable(torch.Tensor([0.5]), requires_grad=True)
+    w8 = Variable(torch.Tensor([0.9]), requires_grad=True)
+    w9 = Variable(torch.Tensor([0.2]), requires_grad=True)
 
     b1 = Variable(torch.Tensor([1.0]))
     b2 = Variable(torch.Tensor([1.0]))
 
     # TODO: Define network forward pass connectivity
     m = Sigmoid()
-    h1 = m(w135).dot(x)
-    h2 = m(w246).dot(x)
     error = inf
     lr = 0.01
 
@@ -47,20 +41,27 @@ def toyNetwork():
     # 4 - Zero weight gradients with w_.grad_zero_()
 
     while error > 0.001:
-        y_hat = torch.Tensor((h1,h2,b2)).dot(w789)
+        w135 = Variable(torch.Tensor([w1,w3,w5]), requires_grad=True)
+        w246 = Variable(torch.Tensor([w2,w4,w6]), requires_grad=True)
+        h1 = m(w135.dot(x))
+        h2 = m(w246.dot(x))
+        y_hat = w7*h1+w8*h2+w9
         error = torch.sum( torch.pow(y - y_hat,2))
         error.backward()
 
         with torch.no_grad():
             w135 -= lr * w135.grad
             w246 -= lr * w246.grad
-            w789 -= lr * w789.grad
+            w7 -= lr * w7.grad
+            w8 -= lr * w8.grad
+            w9 -= lr * w9.grad
             w135.grad.zero_()
             w246.grad.zero_()
-            w789.grad.zero_()
+            w7.grad.zero_()
+            w8.grad.zero_()
+            w9.grad.zero_()
 
         print("Error: {:.4f}".format(error))
-
 
 
 if __name__ == "__main__":
