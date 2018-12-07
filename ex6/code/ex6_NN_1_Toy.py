1ade7298990bf48ce21d22cc6009c366edf82c3a
Burgert Maximilian - burmax00 <max.burgert@unibas.ch>
2018-12-07 13:29:59 +0100

    Added gradient output for weights

--- ex6/code/ex6_NN_1_Toy.py
+++ ex6/code/ex6_NN_1_Toy.py
@@ -31,7 +31,7 @@ def toyNetwork():
     # TODO: Define network forward pass connectivity
     m = Sigmoid()
     error = inf
-    lr = 0.01
+    lr = 0.2
 
     # TODO: Train network until convergence
     # Steps:
@@ -40,7 +40,7 @@ def toyNetwork():
     # 3 - update weight variables according to gradient and learning rate
     # 4 - Zero weight gradients with w_.grad_zero_()
 
-    while error > 0.001:
+    while error > 0.0001:
         w135 = Variable(torch.Tensor([w1,w3,w5]), requires_grad=True)
         w246 = Variable(torch.Tensor([w2,w4,w6]), requires_grad=True)
         h1 = m(w135.dot(x))
@@ -51,10 +51,15 @@ def toyNetwork():
 
         with torch.no_grad():
             w135 -= lr * w135.grad
+            print("w1: {:.4f}\nw3: {:.4f}\nw4: {:.4f}".format(w135.grad[0],w135[1],w135[2]))
             w246 -= lr * w246.grad
+            print("w2: {:.4f}\nw4: {:.4f}\nw6: {:.4f}".format(w246.grad[0], w246[1], w246[2]))
             w7 -= lr * w7.grad
             w8 -= lr * w8.grad
             w9 -= lr * w9.grad
+            print("w7 grad: {:.4f}".format(w7.grad.item()))
+            print("w8 grad: {:.4f}".format(w8.grad.item()))
+            print("w9 grad: {:.4f}".format(w9.grad.item()))
             w135.grad.zero_()
             w246.grad.zero_()
             w7.grad.zero_()
@@ -70,3 +75,45 @@ if __name__ == "__main__":
     print("Neural network toy example!")
     toyNetwork()
     print("Done!")
+
+    # w1: 0.0304
+    # w3: 0.2939
+    # w4: 0.7939
+    # w2: 0.0868
+    # w4: 0.0826
+    # w6: 0.2826
+    # w7
+    # grad: 0.3617
+    # w8
+    # grad: 0.2905
+    # w9
+    # grad: 0.4348
+    # Error: 0.0473
+    # w1: 0.0038
+    # w3: 0.2992
+    # w4: 0.7992
+    # w2: 0.0117
+    # w4: 0.0977
+    # w6: 0.2977
+    # w7
+    # grad: 0.0523
+    # w8
+    # grad: 0.0420
+    # w9
+    # grad: 0.0628
+    # Error: 0.0010
+    # w1: 0.0005
+    # w3: 0.2999
+    # w4: 0.7999
+    # w2: 0.0017
+    # w4: 0.0997
+    # w6: 0.2997
+    # w7
+    # grad: 0.0076
+    # w8
+    # grad: 0.0061
+    # w9
+    # grad: 0.0091
+    # Error: 0.0000
+    # Done!
+
